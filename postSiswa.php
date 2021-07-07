f7fb417836b444e921ea2cdfcf495491560665ce
asropinr <asropinr@gmail.com>
2021-07-07 18:42:11 +0700

    update

--- postSiswa.php
+++ postSiswa.php
@@ -1,6 +1,6 @@
 <?php
 include 'koneksi.php';
-$result = pg_query($conn, "select * from jurusan ");
+$result = pg_query($conn, "SELECT j.id, j.nama, j.kuotamax, count( dj.id_siswa) as jumlah FROM jurusan j JOIN detailjurusan dj ON j.id = dj.id_jur GROUP BY j.id ");
 
 $arr = pg_fetch_all($result);
 $max = count($arr);
@@ -10,6 +10,7 @@ for($i=0;$i<$max;$i++){
         'id' => $arr[$i]["id"],
         'nama' => $arr[$i]["nama"],
         'kuota' => $arr[$i]["kuotamax"],
+        'jumlah_sekarang' => $arr[$i]["jumlah"]
         ];
 }
 $data = ['data' => $json];
