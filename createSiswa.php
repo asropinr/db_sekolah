4b1916b1e95047185711276e297fc6728717f8c7
asropinr <asropinr@gmail.com>
2021-07-07 19:41:48 +0700

    dev

--- createSiswa.php
+++ createSiswa.php
@@ -1,6 +1,6 @@
 <?php 
 include 'koneksi.php';
-$sql = "insert into test(id, nama, image,alamat,notlep,sekolah_asal,nama_ortu,NISN)  
+$sql = "insert into siswa(id, nama, image,alamat,notlep,sekolah_asal,nama_ortu,NISN)  
 values('$_POST[id]', '$_POST[nama]', '$_POST[image]','$_POST[alamat]','$_POST[notelp]','$_POST[sekolah_aasl]','$_POST[nama_ortu]','$_POST[NISn]')";
 $result = pg_query($dbconn, $sql);
 if($result){
