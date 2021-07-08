7abf6e06a006653453266cc92d54918640657303
asropinr <asropinr@gmail.com>
2021-07-08 09:02:45 +0700

    dev

--- createDetail.php
+++ createDetail.php
@@ -1,15 +1,8 @@
-<?php 
+<?php
 include 'koneksi.php';
-// $sql = "insert into siswa(id, nama, image,alamat,notlep,sekolah_asal,nama_ortu,NISN)  
-// values('$_POST[id]', '$_POST[nama]', '$_POST[image]','$_POST[alamat]','$_POST[notelp]','$_POST[sekolah_aasl]','$_POST[nama_ortu]','$_POST[NISn]')";
-// $result = pg_query($conn, $sql);
-// if($result){
-//   echo "Berhasil";
-// }
-// SET HEADER
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
@@ -17,9 +10,9 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $msg['message'] = '';
 
 // CHECK IF RECEIVED DATA FROM THE REQUEST
-if($_GET['id']){
+if($_GET['id_siswa']){
     // CHECK DATA VALUE IS EMPTY OR NOT
-      $query = "update siswa set nama = '$_GET[nama]',image = '$_GET[image]',alamat = '$_GET[alamat]',notelp = $_GET[notelp],sekolah_asal = '$_GET[sekolah_asal]',nama_ortu = '$_GET[nama_ortu]',nisn = '$_GET[nisn]' where id = '$_GET[id]'";
+      $query = "insert into detailjurusan(id_siswa, id_jur) values('$_GET[id_siswa]', '$_GET[id_jur]')";
       $result = pg_query($query); 
       if($result){
         $msg['message'] = "SUKSES";
